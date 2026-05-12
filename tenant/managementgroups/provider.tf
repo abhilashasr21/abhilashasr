e1f1efe635c2113c6a8513023921b59f35285d9a
abhilashasr21 <abhilashasr@microsoft.com>
2026-05-12 21:52:55 +0530

    adding code

---
+++ tenant/managementgroups/provider.tf
@@ -0,0 +1,23 @@
+# Configure Terraform to use the Azure provider
+
+terraform {
+  required_version = ">= 1.5.0, < 2.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0, < 4.0.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "my-tfstate-rg"
+    storage_account_name = "mytfstateacct2026"
+    container_name       = "tfstate"
+    key                  = "management_groups.tfstate"
+  }
+}
+
+# Configure the Microsoft Azure Provider 
+provider "azurerm" {
+  features {}
+}
