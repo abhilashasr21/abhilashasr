7966043ddb8d75d37bbe17372ad1fcf8c05ef699
abhilashasr21 <abhilashasr@microsoft.com>
2026-05-13 13:58:24 +0530

    adding management groups

---
+++ tenant/bu0001/bu0001.tfvars
@@ -0,0 +1,6 @@
+"management_groups" = {
+  "root" = {
+    "display_name" = "Root Management Group"
+    "name" = "root"
+  }
+}
--- tenant/managementgroups/main.tf
+++ tenant/managementgroups/main.tf
@@ -1,13 +1,3 @@
-# 
-#create Variable for management groups
-variable "management_groups" {
-  description = "A map of management groups to create. The key is the name of the management group, and the value is an object containing the display name and parent ID."
-  type = map(object({
-    display_name = string
-    parent_id    = string
-  }))
-}
-
 #create block for management group tf resource
 resource "azurerm_management_group" "parent" {
   for_each     = var.management_groups
